81bc1f1ba5d56b65a5164b1a3ca038cea7b1479c
eunhcho <eunhcho@student.42seoul.kr>
2024-01-12 21:44:21 +0900

    fixed map check

--- srcs_b/check_map_bonus.c
+++ srcs_b/check_map_bonus.c
@@ -6,49 +6,41 @@
 /*   By: eunhcho <eunhcho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 19:57:34 by eunhcho           #+#    #+#             */
-/*   Updated: 2024/01/12 19:37:17 by eunhcho          ###   ########.fr       */
+/*   Updated: 2024/01/12 21:43:33 by eunhcho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3D_bonus.h"
 
-static void	check_boundary(t_game *game, int x, int y, char **map)
+static void	check_zero(t_game *game, int x, int y, char **map)
 {
-	if (y == 0 || y == game->map->height - 1)
+	if (x == 0 || y == 0 || x == game->map->width[y] - 1 || y == game->map->height - 1)
+		ft_error("Error\nMap is not surrounded by wall 1\n", game);
+	if (y - 1 >= 0 && x > game->map->width[y - 1])
+		ft_error("Error\nMap is not surrounded by wall 2\n", game);
+	if (y + 1 < game->map->height && x > game->map->width[y + 1])
+		ft_error("Error\nMap is not surrounded by wall 3\n", game);
+	if (x - 1 >= 0 && y - 1 >= 0)
 	{
-		if (map[y][x] == '0')
-			ft_error("Error\nInvalid map check\n", game);
+		if (map[y][x - 1] == ' ' || map[y - 1][x] == ' ')
+			ft_error("Error\nMap is not surrounded by wall 4\n", game);
 	}
-	else if (x == 0 || x == game->map->width[y] - 1)
+	if (x + 1 < game->map->width[y] && y + 1 < game->map->height)
 	{
-		if (map[y][x] == '0')
-			ft_error("Error\nInvalid map check\n", game);
+		if (map[y][x + 1] == ' ' || map[y + 1][x] == ' ')
+			ft_error("Error\nMap is not surrounded by wall 5\n", game);
 	}
 }
 
-static void	check_adjacent(t_game *game, int x, int y, char **map)
-{
-	if (map[y][x] == '0')
-	{
-		if (y - 1 >= 0 && y + 1 <= game->map->height - 1 && \
-			x - 1 >= 0 && x + 1 <= game->map->width[y])
-		{
-			if (map[y][x + 1] == ' ' || map[y][x - 1] == ' ' || \
-				map[y + 1][x] == ' ' || map[y - 1][x] == ' ')
-				ft_error("Error\nInvalid map check\n", game);
-		}
-	}
-}
-
-void	check_cell(t_game *game, int x, int y, char **map)
+static void	check_cell(t_game *game, int x, int y, char **map)
 {
 	while (y < game->map->height)
 	{
 		x = 0;
 		while (x < game->map->width[y])
 		{
-			check_boundary(game, x, y, map);
-			check_adjacent(game, x, y, map);
+			if (map[y][x] == '0' || map[y][x] == '2' || map[y][x] == '4')
+				check_zero(game, x, y, map);
 			x++;
 		}
 		y++;
