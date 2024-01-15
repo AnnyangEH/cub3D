3fa64ecd6dc5800b7f4408b2ea15f9b65c86a388
eunhcho <eunhcho@student.42seoul.kr>
2024-01-15 21:34:47 +0900

    really really done

--- srcs_m/main.c
+++ srcs_m/main.c
@@ -6,22 +6,16 @@
 /*   By: eunhcho <eunhcho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 19:32:44 by eunhcho           #+#    #+#             */
-/*   Updated: 2024/01/15 21:09:52 by eunhcho          ###   ########.fr       */
+/*   Updated: 2024/01/15 21:34:14 by eunhcho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3D.h"
 
-static void	leak(void)
-{
-	system("leaks cub3D");
-}
-
 int	main(int ac, char **av)
 {
 	t_game	game;
 
-	atexit(leak);
 	ft_init(ac, av, &game);
 	parse(&game);
 	mlx_hook(game.win, 17, 0, exit_hook, &game);
